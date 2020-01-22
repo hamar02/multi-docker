4c0666edcb58a3229712be1c3c4662103de35164
hampuz <hampuz.aronsson@gmail.com>
2020-01-22 21:42:14 +0100

    docker user is not travis user..

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t hamar02/multi-client:latest -t hamar02/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t hamar02/multi-server:latest -t hamar02/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t hamar02/multi-worker:latest -t hamar02/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t hamaro/multi-client:latest -t hamaro/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t hamaro/multi-server:latest -t hamaro/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t hamaro/multi-worker:latest -t hamaro/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push hamar02/multi-client:latest
-docker push hamar02/multi-client:$SHA
-docker push hamar02/multi-server:latest
-docker push hamar02/multi-server:$SHA
-docker push hamar02/multi-worker:latest
-docker push hamar02/multi-worker:$SHA
+docker push hamaro/multi-client:latest
+docker push hamaro/multi-client:$SHA
+docker push hamaro/multi-server:latest
+docker push hamaro/multi-server:$SHA
+docker push hamaro/multi-worker:latest
+docker push hamaro/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=hamar02/multi-server:$SHA
-kubectl set image deployments/client-deployment client=hamar02/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=hamar02/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=hamaro/multi-server:$SHA
+kubectl set image deployments/client-deployment client=hamaro/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=hamaro/multi-worker:$SHA
